2d89fa0202a03707f16532c98e2ad21305570065
shivkumar-github <shivkumarrajmane164@gmail.com>
2024-04-26 09:36:44 +0530

    pushing some codes of hashing and maps  Please enter the commit message for your changes. Lines starting

--- dsa love babbar/Lecture19 C++ STL in 1 Video (Re-Uploaded)/unordered_map_and_map.cpp
+++ dsa love babbar/Lecture19 C++ STL in 1 Video (Re-Uploaded)/unordered_map_and_map.cpp
@@ -2,35 +2,37 @@
 
 */
 
-#include<iostream>
-#include<unordered_map>
-#include<map>
+#include <iostream>
+#include <unordered_map>
+#include <map>
 using namespace std;
-int main(){
-    unordered_map<int, string> studentsum;
-    map<int, string> studentsm;
-    pair<int,string> p1(1, "shivkumar1");
-    pair<int,string> p3(3, "shivkumar3");
-    pair<int,string> p2(2, "shivkumar2");
-    studentsum.insert(p1);
-    studentsum.insert(p3);
-    studentsum.insert(p2);
-    studentsm.insert(p1);
-    studentsm.insert(p3);
-    studentsm.insert(p2);
+int main()
+{
+    unordered_map<int, string> students_unordered;
+    map<int, string> students_ordered;
+    pair<int, string> p1(1, "shivkumar1");
+    pair<int, string> p3(3, "shivkumar3");
+    pair<int, string> p2(2, "shivkumar2");
+    students_unordered.insert(p1);
+    students_unordered.insert(p3);
+    students_unordered.insert(p2);
+    students_ordered.insert(p1);
+    students_ordered.insert(p3);
+    students_ordered.insert(p2);
 
-    // for (auto i : studentsm )
-    // {
-    //     cout << i.first << " ";
-    //     cout<< i.second << endl;
-    // }
-    
-    // for (auto i : studentsum )
-    // {
-    //     cout << i.first << " ";
-    //     cout<< i.second << endl;
-    // }
+    for (auto i : students_ordered) // printed according to key values
+    {
+        cout << i.first << " ";
+        cout << i.second << endl;
+    }
+    cout << endl;
 
-    cout<< studentsm.front() << endl;
-return 0;
+    for (auto i : students_unordered) // printed as inserted
+    {
+        cout << i.first << " ";
+        cout << i.second << endl;
+    }
+
+    // cout<< students_ordered.front() << endl;
+    return 0;
 }
