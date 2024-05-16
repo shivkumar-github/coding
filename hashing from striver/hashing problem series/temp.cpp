53cb20b82a53e576be0133c6dfc5519e496554c8
shivkumar-github <shivkumarrajmane164@gmail.com>
2024-05-16 21:15:05 +0530

    updating the codes

--- hashing from striver/hashing problem series/temp.cpp
+++ hashing from striver/hashing problem series/temp.cpp
@@ -1,20 +1,23 @@
 /*
 
 */
-
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <unordered_map>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
 public:
+        double power(int num, int index)
+        {
+                double ans = 1;
+                for (int i = 0; i < index; i++)
+                {
+                        ans *= num;
+                }
+                return ans;
+        }
         string intToRoman(int num)
         {
-                cout << "hey" << endl;
                 string ans = "";
                 unordered_map<int, char> mpp;
                 mpp.insert({1, 'I'});
@@ -32,52 +35,55 @@ public:
                         temp /= 10;
                 }
                 temp = num;
-                unordered_map<int, int> nummp;
+                map<int, int> nummp;
                 for (int i = 0; i < ndigit; i++)
                 {
-                        int place = double(pow(10, ndigit - i - 1));
+                        int place = int(power(10, ndigit - i - 1));
                         nummp[place] = temp / place;
-                        temp /= 10;
+                        cout<< temp << endl;
+                        temp %= place;
                 }
                 unordered_map<int, vector<char>> requirments;
                 requirments.insert({1, {'I', 'V', 'X'}});
                 requirments.insert({10, {'X', 'L', 'C'}});
                 requirments.insert({100, {'C', 'D', 'M'}});
-                requirments.insert({100, {'M'}});
-                for (auto i : nummp)
+                requirments.insert({1000, {'M'}});
+                for (auto i = nummp.rbegin(); i != nummp.rend(); i++)
                 {
-                        int place = i.first;
-                        int digit = i.second;
+                        int place = i->first;
+                        int digit = i->second;
                         if (digit == 0)
                         {
                                 // do  nothing
                         }
-                        else if (digit <=3)
+                        else if (digit <= 3)
                         {
                                 for (int j = 0; j < digit; j++)
                                 {
                                         ans += requirments[place][0];
-                                }                                
+                                }
                         }
-                        else if (digit==4)
+                        else if (digit == 4)
                         {
-                                ans += requirments[place][0] + requirments[place][1];
+                                ans += requirments[place][0];
+                                ans += requirments[place][1];
                         }
-                        else if (digit==5)
+                        else if (digit == 5)
                         {
                                 ans += requirments[place][1];
                         }
-                        else if (digit<=8)
+                        else if (digit <= 8)
                         {
                                 ans += requirments[place][1];
-                                 for (int j = 0; j < digit-5; j++)
+                                for (int j = 0; j < digit - 5; j++)
                                 {
                                         ans += requirments[place][0];
-                                }        
+                                }
                         }
-                        else if (digit==9)
+                        else if (digit == 9)
                         {
-                                ans += requirments[place][0] + requirments[place][2];
+                                ans += requirments[place][0];
+                                ans += requirments[place][2];
                         }
                 }
                 return ans;
@@ -86,12 +92,11 @@ public:
 int main()
 {
 
-        int num;
-        cin >> num;
+        int num = 3999;
+        // cin >> num;
         Solution s;
-        cout<< "hello world" << endl;
         string ans = s.intToRoman(num);
         cout << ans << endl;
-  
-                return 0;
+
+        return 0;
 }
